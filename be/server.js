2f2d1c8e9fcab83e0d4c2cc8f1cbd029e9673d31
Ben Burgos <ben.a.burgos@gmail.com>
2024-01-25 20:30:00 -0700

    init commit

---
+++ be/server.js
@@ -0,0 +1,9 @@
+// Dependencies
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// Listener
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}!`)
+);
