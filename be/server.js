c2a6173c9f1c0f82f7db742bcfa256d4ea583ec5
Ben Burgos <ben.a.burgos@gmail.com>
2024-01-25 20:41:46 -0700

    adds mongoose

--- be/server.js
+++ be/server.js
@@ -1,7 +1,17 @@
 // Dependencies
+require('dotenv').config();
 const express = require('express');
 const app = express();
-require('dotenv').config();
+const mongoose = require('mongoose');
+
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL);
+
+// DB Connection Events
+const db = mongoose.connection;
+db.on('error', (err) => console.log('Error:' + err.message));
+db.on('connected', () => console.log('Connected to database.'));
+db.on('disconnected', () => console.log('Disconnected from database.'));
 
 // Listener
 app.listen(process.env.PORT, () =>
