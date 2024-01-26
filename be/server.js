b25573b58a82b79cd7ebc46d0bdca6e3ab48fe08
Ben Burgos <ben.a.burgos@gmail.com>
2024-01-25 21:53:24 -0700

    completes basic create in db

--- be/server.js
+++ be/server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+// Middleware
+app.use(express.json());
+
 // Route Dependencies
 const User = require('./models/user.js');
-
-// Middleware
-app.use(express.urlencoded({ extended: true }));
 
 // Database Connection
 mongoose.connect(process.env.DATABASE_URL);
@@ -21,12 +21,12 @@ db.on('disconnected', () => console.log('Disconnected from database.'));
 
 // Routes / Controllers
 // Create Route
-app.post('/user', (req, res) => {
-  req.body.isAdmin ? (req.body.isAdmin = 'Yes') : (req.body.isAdmin = 'No');
-
-  User.create(req.body, (error, createdUser) => {
-    res.send(createdUser);
-  });
+app.post('/users', (req, res) => {
+  db.collection('users').insertOne(req.body, (err, res) => {
+    if (err) throw err;
+    console.log('New user created.')
+  })
+  res.send(`New user ${req.body.firstName} created.`)
 });
 
 // Listener
