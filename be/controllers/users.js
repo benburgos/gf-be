3d5bba3fc0122daa833f339a1caa9b9029245a57
Ben Burgos <ben.a.burgos@gmail.com>
2024-01-31 21:08:59 -0700

    imports existing functions from server

--- be/controllers/users.js
+++ be/controllers/users.js
@@ -1,6 +1,36 @@
 const db = require('../config/db');
 const User = require('../models/user');
+const { v4: uuidv4 } = require('uuid');
 
+// Create User
+async function createUser(req, res) {
+  req.body = {
+    ...req.body,
+    _id: uuidv4(),
+    userID: uuidv4(),
+    brandID: uuidv4(),
+    dateUpdated: Date.now(),
+    dateCreated: Date.now(),
+  };
+  try {
+    const user = await User.create(req.body);
+    res.send(`New user, ${user.firstName}, was created.`);
+  } catch (err) {
+    res.send(err);
+  }
+}
+
+// Get Single User
+async function getUser(req, res) {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    res.send(user);
+  } catch (err) {
+    res.send(err);
+  }
+}
+
+// Get All Users
 async function getUsers(req, res) {
   try {
     const allUsers = await User.find({});
@@ -11,5 +41,7 @@ async function getUsers(req, res) {
 }
 
 module.exports = {
+  createUser,
+  getUser,
   getUsers,
 };
