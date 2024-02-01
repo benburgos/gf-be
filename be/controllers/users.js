97edc1b934a8dce98bcb6bd0ac24392e71ab0224
Ben Burgos <ben.a.burgos@gmail.com>
2024-01-31 20:40:28 -0700

    fixes user model callstack

--- be/controllers/users.js
+++ be/controllers/users.js
@@ -1,15 +1,15 @@
-const db = require('../config/db')
-const User = require('./models/user.js');
+const db = require('../config/db');
+const User = require('../models/user');
 
 async function getUsers(req, res) {
-    try {
-        const allUsers = await User.find({});
-        res.send(allUsers);
-      } catch (err) {
-        throw err;
-      }
+  try {
+    const allUsers = await User.find({});
+    res.send(allUsers);
+  } catch (err) {
+    throw err;
+  }
 }
 
 module.exports = {
-    getUsers,
-}
+  getUsers,
+};
