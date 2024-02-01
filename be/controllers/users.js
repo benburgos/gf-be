6baba7382ffe789d609bdea98b1ca45b3dfda2d3
Ben Burgos <ben.a.burgos@gmail.com>
2024-01-31 20:37:36 -0700

    adds getUsers function

---
+++ be/controllers/users.js
@@ -0,0 +1,15 @@
+const db = require('../config/db')
+const User = require('./models/user.js');
+
+async function getUsers(req, res) {
+    try {
+        const allUsers = await User.find({});
+        res.send(allUsers);
+      } catch (err) {
+        throw err;
+      }
+}
+
+module.exports = {
+    getUsers,
+}
