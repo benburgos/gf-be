ccb156e6d9402e41171f18621ad75c0492990e8a
Ben Burgos <ben.a.burgos@gmail.com>
2024-01-31 21:10:36 -0700

    adds logic for get single and create

--- be/routes/users.js
+++ be/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users');
 
+router.post('/', usersController.createUser)
+router.get('/:id', usersController.getUser)
 router.get('/', usersController.getUsers);
 
 module.exports = router;
