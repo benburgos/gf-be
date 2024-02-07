f21485faa83df2dbf9a035e9020f2b5167f81b45
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-06 21:57:15 -0700

    creates verification middleware

--- be/controllers/login.js
+++ be/controllers/login.js
@@ -1,18 +1,15 @@
 const db = require('../config/db');
 const User = require('../models/user');
 const Pwh = require('../models/pwh');
-const bcrypt = require('bcrypt');
+const { checkHash } = require('../middlewares/checkHash');
 
 async function loginUser(req, res) {
   try {
-    const user = await User.findOne({ email: req.body.email });
-    const pw = await Pwh.findOne({ userID: user._id });
-    const match = await bcrypt.compare(req.body.password, pw.pwh);
-
+    const match = await checkHash(req.body.email, req.body.password);
     if (match) {
-      res.send('Successful login.');
+      res.send('Successful Login.');
     } else {
-      res.send('Password incorrect');
+      res.send('Incorrect Password.');
     }
   } catch (err) {
     throw err;
---
+++ be/middlewares/checkHash.js
@@ -0,0 +1,17 @@
+// Write function that will check the entered password against the hashed value in DB for UserID
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const Pwh = require('../models/pwh');
+
+async function checkHash(email, password) {
+  const user = await User.findOne({ email: email });
+  const pw = await Pwh.findOne({ userID: user._id });
+
+  const match = await bcrypt.compare(password, pw.pwh);
+
+  return match;
+}
+
+module.exports = {
+  checkHash,
+};
