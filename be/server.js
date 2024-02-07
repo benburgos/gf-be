4aa7327d277406eff6a3bdf60e70ae88e575f194
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-06 21:48:02 -0700

    adds login route

--- be/server.js
+++ be/server.js
@@ -3,12 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 
 // Middleware
 app.use(express.json());
 
 // Routes
-app.use('/users', usersRouter)
+app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // Listener
 app.listen(process.env.PORT, () =>
