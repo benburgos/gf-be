919968807feef8d5d58f4b6a83f69c4025ded7cb
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-12 20:46:59 -0700

    adds additional data

--- be/middlewares/checkHash.js
+++ be/middlewares/checkHash.js
@@ -6,9 +6,12 @@ async function checkHash(email, password) {
   const user = await User.findOne({ email: email });
   const pw = await Pwh.findOne({ userID: user._id });
 
-  const match = await bcrypt.compare(password, pw.pwh);
+  const isMatch = await bcrypt.compare(password, pw.pwh);
 
-  return match;
+  return {
+    isMatch,
+    user
+  };
 }
 
 module.exports = {
