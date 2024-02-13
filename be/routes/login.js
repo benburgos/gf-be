4fb44c6994925b3d97e68d72758e0ebe83aa8b22
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-12 20:17:55 -0700

    removes get adds post

--- be/routes/login.js
+++ be/routes/login.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const loginController = require('../controllers/login');
 
-router.get('/', loginController.loginUser);
+router.post('/', loginController.loginUser);
 
 module.exports = router;
