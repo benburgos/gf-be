5743bcaa934ee6abb723f27ac6a874d28028bf7c
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-12 21:13:56 -0700

    adds token generator

--- be/controllers/login.js
+++ be/controllers/login.js
@@ -1,9 +1,11 @@
 const { checkHash } = require('../middlewares/checkHash');
+const { genToken } = require('../services/genToken');
 
 async function loginUser(req, res) {
   try {
     const match = await checkHash(req.body.email, req.body.password);
-    if (match) {
+    if (match.isMatch) {
+      genToken(match, match.key);
       res.send('Successful Login.');
     } else {
       res.send('Incorrect Password.');
