213b32c6bdea810490e7ad915237ca15b0b5740f
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-12 22:19:20 -0700

    implements basic auth

--- be/controllers/login.js
+++ be/controllers/login.js
@@ -5,8 +5,7 @@ async function loginUser(req, res) {
   try {
     const match = await checkHash(req.body.email, req.body.password);
     if (match.isMatch) {
-      genToken(match, match.key);
-      res.send('Successful Login.');
+      res.json(genToken(match, match.key));
     } else {
       res.send('Incorrect Password.');
     }
