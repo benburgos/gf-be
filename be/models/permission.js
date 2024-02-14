7800c5ce6a105b5ea426867ad39c8a7774155830
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-13 22:17:34 -0700

    creates permission model

---
+++ be/models/permission.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const permissionSchema = new Schema({
+  _id: { type: String, required: true },
+  type: { type: String, enum: ['r', 'w', 'rw'], required: true },
+  productId: { type: String, required: true },
+  brandId: { type: String, required: true },
+});
+
+const Permission = mongoose.model('Permission', permissionSchema);
+
+module.exports = Permission;
