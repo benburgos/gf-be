d15a1452db97c91701e31a9600cb5863173444eb
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-23 15:10:04 -0700

    completes create route

--- be/controllers/brand.js
+++ be/controllers/brand.js
@@ -3,7 +3,22 @@ const Brand = require('../models/brand');
 const { v4: uuidv4 } = require('uuid');
 
 // Create Brand
-async function createBrand(req, res) {}
+async function createBrand(req, res) {
+  //   const existing = await Brand.find({ name: req.body.name }).collation({
+  //     strength: 1,
+  //   });
+  //   console.log(existing)
+  req.body = {
+    ...req.body,
+    _id: uuidv4(),
+  };
+  try {
+    const brand = await Brand.create(req.body);
+    res.send(brand);
+  } catch (err) {
+    res.send(err);
+  }
+}
 
 // Get Brand
 async function getBrand(req, res) {}
