7c799713aa3e2aad7b6daef497866d8f3d581be8
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-23 15:13:54 -0700

    removes timestamps

--- be/models/brand.js
+++ be/models/brand.js
@@ -6,8 +6,6 @@ const brandSchema = new mongoose.Schema({
   desc: { type: String, required: false },
   imgUrl: { type: String, required: false },
   userCount: { type: Number, default: 0, required: true },
-  dateUpdated: { type: Date, default: Date.now, required: true },
-  dateCreated: { type: Date, default: Date.now, required: true },
 });
 
 const Brand = mongoose.model('Brand', brandSchema);
