04d35fec74b658f60977c1ddeb3ba0e4bf9a9534
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-23 15:10:52 -0700

    adds brand

--- be/server.js
+++ be/server.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 const loginRouter = require('./routes/login');
 const usersRouter = require('./routes/users');
-const pmsRouter = require('./routes/pms')
-const productRouter = require('./routes/product')
+const pmsRouter = require('./routes/pms');
+const productRouter = require('./routes/product');
+const brandRouter = require('./routes/brand');
 
 // Middleware
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/axs', pmsRouter);
 app.use('/api/product', productRouter);
+app.use('/api/brand', brandRouter);
 
 // Listener
 app.listen(process.env.PORT, () =>
