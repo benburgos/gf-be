5e141a86f0831c3eb91a69ad1fa4e63d8dc1fa72
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-23 15:03:49 -0700

    creates init controller

---
+++ be/controllers/brand.js
@@ -0,0 +1,12 @@
+const db = require('../config/db');
+const Brand = require('../models/brand');
+const { v4: uuidv4 } = require('uuid');
+
+// Create Brand
+async function createBrand(req, res) {}
+
+// Get Brand
+async function getBrand(req, res) {}
+
+// Get All Brands
+async function getBrands(req, res) {}
