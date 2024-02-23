fc2819a971be297627977365990eedc94afead5c
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-23 14:20:44 -0700

    creates product model

--- be/models/product.js
+++ be/models/product.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  brandId: { type: String, required: true },
+  desc: {
+    type: String,
+    enum: ['qa', 'pfm', 'eng', 'wfm', 'shop'],
+    required: true,
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
