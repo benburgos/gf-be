645dc02321ff446dba3cbdb9417371d7fd1c3c38
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-23 15:22:51 -0700

    completes create route

--- be/controllers/brand.js
+++ be/controllers/brand.js
@@ -4,21 +4,26 @@ const { v4: uuidv4 } = require('uuid');
 
 // Create Brand
 async function createBrand(req, res) {
-  //   const existing = await Brand.find({ name: req.body.name }).collation({
-  //     strength: 1,
-  //   });
-  //   console.log(existing)
-  req.body = {
-    ...req.body,
-    _id: uuidv4(),
-    dateUpdated: Date.now(),
-    dateCreated: Date.now(),
-  };
-  try {
-    const brand = await Brand.create(req.body);
-    res.send(brand);
-  } catch (err) {
-    res.send(err);
+  const existing = await Brand.findOne({ name: req.body.name }).collation({
+    locale: 'en',
+    strength: 1,
+  });
+
+  if (existing) {
+    res.send(`Brand already exists.`);
+  } else {
+    req.body = {
+      ...req.body,
+      _id: uuidv4(),
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    };
+    try {
+      const brand = await Brand.create(req.body);
+      res.send(brand);
+    } catch (err) {
+      res.send(err);
+    }
   }
 }
 
