7eb0edd5d0ee01e04c135447521eae1e60fcb70c
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-23 14:23:10 -0700

    creates initial functions

---
+++ be/controllers/product.js
@@ -0,0 +1,18 @@
+const db = require('../config/db');
+const Product = require('../models/product')
+const { v4: uuidv4 } = require('uuid');
+
+// Create Product
+async function createProduct(req, res) {
+
+}
+
+// Get Product
+async function getProduct(req, res) {
+
+}
+
+// Get All Products
+async function getProducts(req, res) {
+    
+}
