cd678981ceeffeea9f1658b7d2c1e7bf3edcb7c5
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-23 15:33:36 -0700

    adds name

--- be/models/pms.js
+++ be/models/pms.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const pmsSchema = new mongoose.Schema({
   _id: { type: String, required: true },
+  name: { type: String, required: true },
   type: { type: String, enum: ['r', 'w', 'rw'], required: true },
   productId: { type: String, required: true },
   brandId: { type: String, required: true },
