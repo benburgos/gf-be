55f6f436da41f93ef9b4b147f7e63d186b4a3546
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-26 14:20:51 -0700

    creates product series

---
+++ be/middlewares/createProduct.js
@@ -0,0 +1,26 @@
+const Product = require('../models/product');
+const { v4: uuidv4 } = require('uuid');
+
+async function createProduct(data) {
+  const productList = ['qa', 'pm', 'eng', 'wfm', 'shop'];
+  const products = [];
+
+  for (let i = 0; i < productList.length; i++) {
+    const product = {
+      _id: uuidv4(),
+      brandId: data._id,
+      desc: productList[i],
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    };
+
+    products.push(product);
+  }
+
+  Product.insertMany(products);
+  console.log(`${products.length} products were added to company ${data.name}.`)
+}
+
+module.exports = {
+  createProduct,
+};
