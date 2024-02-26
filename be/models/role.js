b4c087c937a951a9bc7578bd50d0c8f74a629705
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-26 12:39:39 -0700

    creates role model

---
+++ be/models/role.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const roleSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  brandId: { type: String, required: true },
+  name: { type: String, required: true },
+  permissions: [
+    {
+      productId: { type: String, required: true },
+      pId: { type: String, required: true },
+    },
+  ],
+  isActive: { type: Boolean, required: true },
+});
+
+const Role = mongoose.model('Role', roleSchema);
+
+module.exports = Role;
