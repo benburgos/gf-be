e6e55bb5220ab9673dc982a2bb547437c7868466
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-26 14:50:33 -0700

    creates permissions

---
+++ be/middlewares/createPermission.js
@@ -0,0 +1,31 @@
+const Permission = require('../models/pms');
+const { v4: uuidv4 } = require('uuid');
+
+async function createPermissions(brand, product) {
+  const permissionList = ['r', 'w', 'rw'];
+  const permissions = [];
+
+  for (let i = 0; i < product.length; i++) {
+    for (let j = 0; j < permissionList.length; j++) {
+      const permission = {
+        _id: uuidv4(),
+        type: permissionList[j],
+        brandId: brand._id,
+        productId: product[i]._id,
+        dateUpdated: Date.now(),
+        dateCreated: Date.now(),
+      };
+
+      permissions.push(permission);
+    }
+  }
+
+  Permission.insertMany(permissions);
+  console.log(
+    `${permissions.length} permissions were added to company, ${brand.name}.`
+  );
+}
+
+module.exports = {
+  createPermissions,
+};
