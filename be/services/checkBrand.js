278befd3d2404b7bee3626a01b2456a614c0492c
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-26 12:56:12 -0700

    creates async brand check

---
+++ be/services/checkBrand.js
@@ -0,0 +1,19 @@
+const db = require('../config/db');
+const Brand = require('../models/brand');
+
+async function checkBrand(req, res) {
+  const existing = await Brand.findOne({ name: req.body.name }).collation({
+    locale: 'en',
+    strength: 1,
+  });
+
+  if (existing) {
+    res.send(`Brand already exists.`);
+  } else {
+    return existing;
+  }
+}
+
+module.exports = {
+  checkBrand,
+};
