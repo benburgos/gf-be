10a19ae3270c66a77846ef6db3a34e56e2d9297d
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-26 13:54:39 -0700

    adds active flag

--- be/models/user.js
+++ be/models/user.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: false },
   phoneNumber: { type: String, required: false },
   roleId: { type: String, required: true },
+  isActive: { type: Boolean, default: true, required: true },
   dateUpdated: { type: Number, required: true },
   dateCreated: { type: Number, required: true },
 });
