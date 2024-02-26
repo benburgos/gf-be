cbc918a75ff21a5b8241bebd66918f07c111daf2
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-26 13:45:35 -0700

    adds function to create initial brand

---
+++ be/middlewares/createBrand.js
@@ -0,0 +1,26 @@
+const Brand = require('../models/brand');
+const { v4: uuidv4 } = require('uuid');
+
+// Create Brand
+async function createBrand(data) {
+  data = {
+    _id: uuidv4(),
+    name: data.brandName,
+    desc: data.brandDesc,
+    imgUrl: data.imgUrl,
+    userCount: 1,
+    dateUpdated: Date.now(),
+    dateCreated: Date.now(),
+  };
+  try {
+    const brand = await Brand.create(data);
+    console.log(`New brand, ${brand.name} was successfully created.`);
+    return brand;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = {
+  createBrand,
+};
