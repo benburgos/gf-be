342bb2c52e191a6362f8134c15e71029ab5fb4fb
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-28 13:54:49 -0700

    creates user and seed data upon creation

--- be/controllers/users.js
+++ be/controllers/users.js
@@ -4,7 +4,8 @@ const { v4: uuidv4 } = require('uuid');
 const { hashPassword } = require('../middlewares/genHash');
 const { createBrand } = require('../middlewares/createBrand');
 const { createProduct } = require('../middlewares/createProduct');
-const { createPermissions } = require('../middlewares/createPermission')
+const { createPermissions } = require('../middlewares/createPermission');
+const { createRole } = require('../middlewares/createRole');
 const { checkBrand } = require('../services/checkBrand');
 
 // Create User
@@ -18,26 +19,28 @@ async function createUser(req, res) {
     const brand = await createBrand(req.body);
     const product = await createProduct(brand);
     const permissions = await createPermissions(brand, product);
-    //   req.body = {
-    //     ...req.body,
-    //     _id: uuidv4(),
-    //     brandId: uuidv4(),
-    //     dateUpdated: Date.now(),
-    //     dateCreated: Date.now(),
-    //   };
-    //   pw = await hashPassword(req.body.pw);
-    //   try {
-    //     const user = await User.create(req.body);
-    //     const hash = await Pwh.create({
-    //       ...req.body,
-    //       _id: uuidv4(),
-    //       userId: user._id,
-    //       pwh: pw,
-    //     });
-    //     res.send(`New user, ${user.firstName}, was created.`);
-    //   } catch (err) {
-    //     res.send(err);
-    //   }
+    const role = await createRole(brand, permissions);
+      req.body = {
+        ...req.body,
+        _id: uuidv4(),
+        brandId: uuidv4(),
+        roleId: role,
+        dateUpdated: Date.now(),
+        dateCreated: Date.now(),
+      };
+      pw = await hashPassword(req.body.pw);
+      try {
+        const user = await User.create(req.body);
+        const hash = await Pwh.create({
+          ...req.body,
+          _id: uuidv4(),
+          userId: user._id,
+          pwh: pw,
+        });
+        res.send(`New user, ${user.firstName}, was created.`);
+      } catch (err) {
+        res.send(err);
+      }
   }
 }
 
