5cae7c80fde15fe75a80f1858ff55ae06b744695
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-28 14:56:13 -0700

    removes user count, adds api designation

--- be/models/sys/brand.js
+++ be/models/sys/brand.js
@@ -5,7 +5,7 @@ const brandSchema = new mongoose.Schema({
   name: { type: String, required: true },
   desc: { type: String, required: false },
   imgUrl: { type: String, required: false },
-  userCount: { type: Number, default: 0, required: true },
+  apiUser: { type: Boolean, default: false, required: true },
   dateUpdated: { type: Number, required: true },
   dateCreated: { type: Number, required: true },
 });
