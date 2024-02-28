8702be4c138546a5c7dc393a7927aea445a94056
Ben Burgos <ben.a.burgos@gmail.com>
2024-02-28 15:06:13 -0700

    updates getBrand

--- be/controllers/brand.js
+++ be/controllers/brand.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const Pwh = require('../models/pwh');
+const Brand = require('../models/sys/brand')
 const { v4: uuidv4 } = require('uuid');
 const { hashPassword } = require('../middlewares/genHash');
 const { createBrand } = require('../middlewares/sys/createBrand');
@@ -20,37 +21,40 @@ async function newBrand(req, res) {
     const product = await createProduct(brand);
     const permissions = await createPermissions(brand, product);
     const role = await createRole(brand, permissions);
-      req.body = {
+    req.body = {
+      ...req.body,
+      _id: uuidv4(),
+      brandId: brand._id,
+      roleId: role,
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    };
+    pw = await hashPassword(req.body.pw);
+    try {
+      const user = await User.create(req.body);
+      const hash = await Pwh.create({
         ...req.body,
         _id: uuidv4(),
-        brandId: brand._id,
-        roleId: role,
-        dateUpdated: Date.now(),
-        dateCreated: Date.now(),
-      };
-      pw = await hashPassword(req.body.pw);
-      try {
-        const user = await User.create(req.body);
-        const hash = await Pwh.create({
-          ...req.body,
-          _id: uuidv4(),
-          userId: user._id,
-          pwh: pw,
-        });
-        res.send(`New company, ${brand.name}, was created under new company admin, ${user.firstName}.`);
-      } catch (err) {
-        res.send(err);
-      }
+        userId: user._id,
+        pwh: pw,
+      });
+      res.send(
+        `New company, ${brand.name}, was created under new company admin, ${user.firstName}.`
+      );
+    } catch (err) {
+      res.send(err);
+    }
   }
 }
 
 // Get Single User
 async function getBrand(req, res) {
   try {
-    const user = await User.findOne({ _id: req.params.id });
+    const brand = await Brand.findOne({ _id: req.params.id });
+    const user = await User.findOne({ _id: req.id });
     // Shows user who accessed page, may think about creating event log.
-    console.log(`User ${req.id} has viewed ${user.firstName}'s page.`);
-    res.send(user);
+    console.log(`User ${user.firstName} has viewed the company, ${brand.name}'s page.`);
+    res.send(brand);
   } catch (err) {
     res.send(err);
   }
