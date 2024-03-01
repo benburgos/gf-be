7bddf4f5ea94e09cdea30a5666c9e32f7a98c623
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-01 12:24:39 -0700

    reformats route

--- be/routes/app/user.js
+++ be/routes/app/user.js
@@ -6,7 +6,7 @@ const { authToken } = require('../middlewares/authToken');
 router.post('/create', authToken, userController.createUser);
 router.get('/:id', authToken, userController.getUser);
 router.get('/', authToken, userController.getAllUsers);
-router.put('/:id/edit', authToken, userController.editUser);
-router.delete('/:id', authToken, userController.deleteUser);
+router.put('/edit/:id', authToken, userController.editUser);
+router.delete('/delete/:id', authToken, userController.deleteUser);
 
 module.exports = router;
