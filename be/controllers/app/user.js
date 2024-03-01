c170d221571cb13c661317240a4e73fa52a70d1b
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-01 12:41:12 -0700

    creates user creation route

--- be/controllers/app/user.js
+++ be/controllers/app/user.js
@@ -6,7 +6,38 @@ const { checkBrand } = require('../../services/checkBrand');
 const { checkEmail } = require('../../services/checkEmail');
 
 async function createUser(req, res) {
-  console.log(req.bid)
+  const emailCheck = await checkEmail(req.body.email)
+
+  if (emailCheck){
+    res.send(`The email you're attempting to register already exists.`);
+    return;
+  } else {
+    req.body = {
+      ...req.body,
+      _id: uuidv4(),
+      brandId: req.bid,
+      roleId: req.body.roleId,
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    };
+
+    pw = await hashPassword(req.body.pw);
+
+    try {
+      const user = await User.create(req.body);
+      await Pwh.create({
+        ...req.body,
+        _id: uuidv4(),
+        userId: user._id,
+        pwh: pw,
+      });
+      res.send(
+        `New employee, ${user.firstName}, was added to the database.`
+      );
+    } catch (err) {
+      res.send(err);
+    }
+  }
 }
 async function getUser(req, res) {}
 async function getAllUsers(req, res) {}
