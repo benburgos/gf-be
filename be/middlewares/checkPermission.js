8c7819f8b1c8acc6552d83441bb4bfb96a24b19c
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-04 12:27:32 -0700

    adds conditional

--- be/middlewares/checkPermission.js
+++ be/middlewares/checkPermission.js
@@ -3,9 +3,16 @@ const Permission = require('../models/sys/permission');
 
 async function checkPermission(data) {
   const pId = await Product.findOne({ brandId: data.bid, desc: data.prod });
-  const perm = await Permission.findOne({ productId: pId._id , brandId: data.bid});
+  const perm = await Permission.findOne({
+    productId: pId._id,
+    brandId: data.bid,
+  });
 
-  return perm.type
+  if (pId && perm) {
+    return perm.type;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
