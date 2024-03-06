621226b3b0004d9ea3bf3de492e0daee28cd3680
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-06 16:48:17 -0700

    creates shortAnswer model

--- be/models/qa/options/shortAnswer.js
+++ be/models/qa/options/shortAnswer.js
@@ -1,5 +1,9 @@
-const TrueFalse = {
+const shortAnswer = {
+  options: {
+    position: 0,
+    desc: 'Feedback',
+    value: null,
+  },
+};
 
-}
-
-module.exports = TrueFalse;
+module.exports = shortAnswer;
