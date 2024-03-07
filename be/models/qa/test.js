aeb4de1afdb805a3569249d8fefb0808a5fb0c11
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-07 16:13:47 -0700

    adds a full test of scorecard creation

--- be/models/qa/test.js
+++ be/models/qa/test.js
@@ -55,8 +55,10 @@ option3 = {
 section1 = {
   sectionId: uuidv4(),
   sectionName: 'Call Introduction',
+  position: 0,
   questions: [
     {
+      position: 0,
       questionId: uuidv4(),
       questionName: 'Proper Greeting',
       questionDesc:
@@ -65,14 +67,15 @@ section1 = {
       options: option1.desc,
     },
     {
+      position: 1,
       questionId: uuidv4(),
       questionName: 'Verification',
-      questionDesc:
-        'Agent property verified the caller per company standards.',
+      questionDesc: 'Agent property verified the caller per company standards.',
       optionName: option1.name,
       options: option1.desc,
     },
     {
+      position: 2,
       questionId: uuidv4(),
       questionName: 'Probing Questions',
       questionDesc:
@@ -83,64 +86,83 @@ section1 = {
   ],
 };
 section2 = {
-    sectionId: uuidv4(),
+  sectionId: uuidv4(),
   sectionName: 'Call Body',
+  position: 1,
   questions: [
     {
+      position: 0,
       questionId: uuidv4(),
-      questionName: 'Proper Greeting',
+      questionName: 'Process & Procedure',
       questionDesc:
-        'Agent utilized the propery greeting with company branding.',
-      optionName: option1.name,
-      options: option1.desc,
+        'Agent utilized the proper process for the type of customer issue, within company procedures and guidelines.',
+      optionName: option2.name,
+      options: option2.desc,
     },
     {
+      position: 1,
       questionId: uuidv4(),
-      questionName: 'Verification',
+      questionName: 'Call Control',
       questionDesc:
-        'Agent property verified the caller per company standards.',
-      optionName: option1.name,
-      options: option1.desc,
+        'Agent limits small talk to a minimum to establish a rapport, remaining focused on issue resolution.',
+      optionName: option3.name,
+      options: option3.desc,
     },
     {
+      position: 2,
       questionId: uuidv4(),
-      questionName: 'Probing Questions',
+      questionName: 'Soft Skills',
       questionDesc:
-        'Agent asked probing questions to identify the root cause of the customer issue.',
+        'Agent properly used the preferred name of the customer, limited the use of "uh" and "um", and refrained from using any internal company jargon.',
       optionName: option3.name,
       options: option3.desc,
     },
   ],
 };
 section3 = {
-    sectionId: uuidv4(),
-  sectionName: 'Call Introduction',
+  sectionId: uuidv4(),
+  sectionName: 'Call Close',
+  position: 2,
   questions: [
     {
+      position: 0,
       questionId: uuidv4(),
-      questionName: 'Proper Greeting',
-      questionDesc:
-        'Agent utilized the propery greeting with company branding.',
+      questionName: 'Issue Resolution',
+      questionDesc: 'Agent was able to solve the issue during the phone call.',
       optionName: option1.name,
       options: option1.desc,
     },
     {
+      position: 1,
       questionId: uuidv4(),
-      questionName: 'Verification',
+      questionName: 'Call Recap & Additional Assistance',
       questionDesc:
-        'Agent property verified the caller per company standards.',
-      optionName: option1.name,
-      options: option1.desc,
-    },
-    {
-      questionId: uuidv4(),
-      questionName: 'Probing Questions',
-      questionDesc:
-        'Agent asked probing questions to identify the root cause of the customer issue.',
+        'Agent performed a recap of the call, any applicable next steps, and provided an offer of additional assistance.',
       optionName: option3.name,
       options: option3.desc,
+    },
+    {
+      position: 2,
+      questionId: uuidv4(),
+      questionName: 'Close of Call',
+      questionDesc:
+        'Agent thanked the customer for calling in and pitched the CSAT survey.',
+      optionName: option1.name,
+      options: option1.desc,
     },
   ],
 };
 
-console.log(section1.questions[0].optionType);
+console.log(`Pre-Option: `, newScorecard);
+
+newScorecard.criteria.push(section1);
+newScorecard.criteria.push(section2);
+newScorecard.criteria.push(section3);
+
+console.log(`Post-Option:`, newScorecard);
+
+console.log(`Section 1:`, newScorecard.criteria[0].sectionName);
+console.log(`Section 1, Question 1:`, newScorecard.criteria[0].questions[0].questionName);
+console.log(`Section 1, Question 1, Desc:`, newScorecard.criteria[0].questions[0].questionDesc);
+console.log(`Section 1, Question 1, Option:`, newScorecard.criteria[0].questions[0].optionName);
+console.log(`Section 1, Question 1, Possible Answers:`, newScorecard.criteria[0].questions[0].options);
