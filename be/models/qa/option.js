470f666703af83eed34148613829a9fb7acdb35b
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-07 14:57:51 -0700

    adds option model

--- be/models/qa/option.js
+++ be/models/qa/option.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-const optionSchema = new mongoose.Schema({});
+const optionSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  desc: { type: Array, required: true },
+  dateUpdated: { type: Number, required: true },
+  dateCreated: { type: Number, required: true },
+});
 
 const Option = mongoose.model('Option', optionSchema);
 
