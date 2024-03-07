6881a681013f82bbc661e4af91d31d9d0ecbd771
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-06 17:05:52 -0700

    renames key

--- be/models/qa/options/trueFalse.js
+++ be/models/qa/options/trueFalse.js
@@ -1,7 +1,7 @@
 const TrueFalse = {
   options: [
     { position: 0, desc: 'True', value: true },
-    { position: 1, value: 'False', value: false },
+    { position: 1, desc: 'False', value: false },
   ],
 };
 
