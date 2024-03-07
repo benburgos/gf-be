38992c69cc25e4944989d150a1385c147dc17dc9
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-07 14:31:23 -0700

    adds section model

--- be/models/qa/section.js
+++ be/models/qa/section.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
-const sectionSchema = new mongoose.Schema({});
+const sectionSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  brandId: { type: String, required: true },
+  name: { type: String, required: true },
+  desc: { type: String, required: true },
+  modality: { type: String, required: true },
+  dateUpdated: { type: Number, required: true },
+  dateCreated: { type: Number, required: true },
+});
 
 const Section = mongoose.model('Section', sectionSchema);
 
