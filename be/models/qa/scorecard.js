cf13da601cbde4b2838bd70fbcd394a0cdc7e4ce
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-07 14:38:37 -0700

    creates scorecard model

--- be/models/qa/scorecard.js
+++ be/models/qa/scorecard.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
-const scorecardSchema = new mongoose.Schema({});
+const scorecardSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  brandId: { type: String, required: true },
+  teamId: { type: String, required: true },
+  type: { type: String, required: true },
+  criteria: { type: Array, required: true },
+  isActive: { type: Boolean, default: false, required: true },
+  dateUpdated: { type: Number, required: true },
+  dateCreated: { type: Number, required: true },
+});
 
 const Scorecard = mongoose.model('Scorecard', scorecardSchema);
 
