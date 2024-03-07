1a8d2c66304e6ebe82ad543000a7fdf9fc5c1361
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-06 17:07:56 -0700

    creates numberScale model

--- be/models/qa/options/numberScale.js
+++ be/models/qa/options/numberScale.js
@@ -1,5 +1,8 @@
-const TrueFalse = {
-
+const numberScale = {
+    options: [
+        { position: 0, desc: 'Min. Value', value: 0 },
+        { position: 1, desc: 'Max Value', value: 10 },
+      ],
 }
 
-module.exports = TrueFalse;
+module.exports = numberScale;
