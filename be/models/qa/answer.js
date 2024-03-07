85fe7efdd82294229c0a6a811559310751692aae
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-06 17:25:12 -0700

    creates answerSchema

--- be/models/qa/answer.js
+++ be/models/qa/answer.js
@@ -1,6 +1,20 @@
 const mongoose = require('mongoose');
 
-const answerSchema = new mongoose.Schema({});
+const answerSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  brandId: { type: String, required: true },
+  type: { type: String, required: true },
+  modality: { type: String, required: true },
+  options: [
+    {
+      position: { type: Number, required: true },
+      desc: { type: String, required: true },
+      value: { type: { String, Number }, required: true },
+    },
+  ],
+  dateUpdated: { type: Number, required: true },
+  dateCreated: { type: Number, required: true },
+});
 
 const Answer = mongoose.model('Answer', answerSchema);
 
