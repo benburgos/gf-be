24c016f0e05e71a15b77748f29487a48cae00c05
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-07 14:32:41 -0700

    adds question model

--- be/models/qa/question.js
+++ be/models/qa/question.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
-const questionSchema = new mongoose.Schema({});
+const questionSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  brandId: { type: String, required: true },
+  name: { type: String, required: true },
+  desc: { type: String, required: true },
+  modality: { type: String, required: true },
+  dateUpdated: { type: Number, required: true },
+  dateCreated: { type: Number, required: true },
+});
 
 const Question = mongoose.model('Question', questionSchema);
 
