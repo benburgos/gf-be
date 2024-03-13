c80269a04292af05a9f4452d03cbba206ac34e28
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-13 15:38:35 -0700

    fixes broken import

--- be/controllers/app/qa/option.js
+++ be/controllers/app/qa/option.js
@@ -1,7 +1,7 @@
 const { Option } = require('../../../models/app/qa/qaIndex')
 const { v4: uuidv4 } = require('uuid');
-const { hashPassword } = require('../../middlewares/genHash');
-const { checkPermission } = require('../../middlewares/checkPermission');
+const { hashPassword } = require('../../../middlewares/genHash');
+const { checkPermission } = require('../../../middlewares/checkPermission');
 
 async function createOption(req, res) {}
 async function getOption(req, res) {}
