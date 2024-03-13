6ca13a13caaf00d53fd158a3b478cdced9a0a181
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-13 15:14:49 -0700

    creates base for option controller

---
+++ be/controllers/app/qa/option.js
@@ -0,0 +1,19 @@
+const Option = require('../../../models/app/qa/option')
+const { v4: uuidv4 } = require('uuid');
+const { hashPassword } = require('../../middlewares/genHash');
+const { checkPermission } = require('../../middlewares/checkPermission');
+
+async function createOption(req, res) {}
+async function getOption(req, res) {}
+async function getOptions(req, res) {}
+async function editOption(req, res) {}
+async function deleteOption(req, res) {}
+
+
+module.exports = {
+    createOption,
+    getOption,
+    getOptions,
+    editOption,
+    deleteOption
+}
