fd4893685a1e84dec364bdcae98cc86fe0eb6118
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-14 14:22:10 -0700

    updates export

--- be/routes/routeIndex.js
+++ be/routes/routeIndex.js
@@ -1,11 +1,13 @@
-const login = require('./login')
-const brand = require('./brand')
-const appUser = require('./app/user')
-const appQaOption = require('./app/qa/option')
+const login = require('./login');
+const brand = require('./brand');
+const appUser = require('./app/user');
+const appQaOption = require('./app/qa/option');
+const appQaQuestion = require('./app/qa/question');
 
 module.exports = {
-    login,
-    brand,
-    appUser,
-    appQaOption
-}
+  login,
+  brand,
+  appUser,
+  appQaOption,
+  appQaQuestion,
+};
