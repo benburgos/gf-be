6d9468bc349c033bff9c4026318ec32a24d68f38
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-14 12:40:40 -0700

    updates option model

--- be/models/app/qa/option.js
+++ be/models/app/qa/option.js
@@ -1,21 +1,36 @@
 const mongoose = require('mongoose');
 
-const optionSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  brandId: { type: String, required: true },
-  name: { type: String, required: true },
-  desc: { type: Array, required: true },
-  modality: { type: String, enum:['phone_inbound', 'phone_outbound', 'chat', 'email', 'other', 'all'], required: true },
-  data: [
-    {
-      position: { type: Number, required: true },
-      name: { type: String, required: true },
-      toolTip: { type: String, required: false },
+const optionSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    brandId: { type: String, required: true },
+    name: { type: String, required: true },
+    desc: { type: String, required: true },
+    modality: {
+      type: String,
+      enum: [
+        'phone_inbound',
+        'phone_outbound',
+        'chat',
+        'email',
+        'other',
+        'all',
+      ],
+      required: true,
     },
-  ],
-  dateUpdated: { type: Number, required: true },
-  dateCreated: { type: Number, required: true },
-});
+    data: [
+      {
+        position: { type: Number, required: true },
+        name: { type: String, required: true },
+        toolTip: { type: String, required: false },
+        _id: false,
+      },
+    ],
+    dateUpdated: { type: Number, required: true },
+    dateCreated: { type: Number, required: true },
+  },
+  { versionKey: false }
+);
 
 const Option = mongoose.model('Option', optionSchema);
 
