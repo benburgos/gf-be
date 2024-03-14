1770ec57d896e672cddfce2bcc6eb205bfcda1bc
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-14 14:35:17 -0700

    tests question

--- be/controllers/app/qa/question.js
+++ be/controllers/app/qa/question.js
@@ -2,7 +2,17 @@ const { Question } = require('../../../models/app/qa/qaIndex');
 const { v4: uuidv4 } = require('uuid');
 const { checkPermission } = require('../../../middlewares/checkPermission');
 
-async function createQuestion(req, res) {}
+async function createQuestion(req, res) {
+    let newQuestion = {
+        _id: uuidv4(),
+        brandId: req.bid,
+        isActive: true,
+        dateCreated: Date.now(),
+        dateUpdated: Date.now()
+    }
+
+    console.log(newQuestion)
+}
 
 async function getQuestion(req, res) {}
 
