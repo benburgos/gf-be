f2fb9887f00aeea04a1cd84675004edb3877c153
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-14 14:45:49 -0700

    adds rbac to createQuestion

--- be/controllers/app/qa/question.js
+++ be/controllers/app/qa/question.js
@@ -3,17 +3,33 @@ const { v4: uuidv4 } = require('uuid');
 const { checkPermission } = require('../../../middlewares/checkPermission');
 
 async function createQuestion(req, res) {
-    let newQuestion = {
-        ...req.body,
-        _id: uuidv4(),
-        brandId: req.bid,
-        isActive: true,
-        dateCreated: Date.now(),
-        dateUpdated: Date.now()
-    }
+  const data = {
+    prod: 'qa',
+    bid: req.bid,
+    ra: req.ra,
+  };
 
-    await Question.create(newQuestion)
-    res.send(newQuestion)
+  const type = await checkPermission(data);
+
+  if (type === 'rw') {
+    let newQuestion = {
+      ...req.body,
+      _id: uuidv4(),
+      brandId: req.bid,
+      isActive: true,
+      dateCreated: Date.now(),
+      dateUpdated: Date.now(),
+    };
+
+    try {
+      await Question.create(newQuestion);
+      res.send(`New question, ${newQuestion.name}, was added to the database.`);
+    } catch (err) {
+      res.send(err);
+    }
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
 }
 
 async function getQuestion(req, res) {}
