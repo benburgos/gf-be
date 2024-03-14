541dcf83fb559d1ea862e00278942245deafc3d7
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-14 11:29:53 -0700

    adds createOption

--- be/controllers/app/qa/option.js
+++ be/controllers/app/qa/option.js
@@ -1,19 +1,45 @@
-const { Option } = require('../../../models/app/qa/qaIndex')
+const { Option } = require('../../../models/app/qa/qaIndex');
 const { v4: uuidv4 } = require('uuid');
-const { hashPassword } = require('../../../middlewares/genHash');
 const { checkPermission } = require('../../../middlewares/checkPermission');
 
-async function createOption(req, res) {}
+async function createOption(req, res) {
+  const data = {
+    prod: 'qa',
+    bid: req.bid,
+    ra: req.ra,
+  };
+
+  const type = await checkPermission(data);
+
+  if (type === 'rw') {
+    let newOption = {
+      _id: uuidv4(),
+      brandId: req.bid,
+      ...req.body,
+      dateCreated: Date.now(),
+      dateUpdated: Date.now(),
+    };
+
+    try {
+      await Option.create(newOption);
+      res.send(`New option, ${newOption.name}, was added to the database.`);
+    } catch (err) {
+      res.send(err);
+    }
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
+}
+
 async function getOption(req, res) {}
 async function getOptions(req, res) {}
 async function editOption(req, res) {}
 async function deleteOption(req, res) {}
 
-
 module.exports = {
-    createOption,
-    getOption,
-    getOptions,
-    editOption,
-    deleteOption
-}
+  createOption,
+  getOption,
+  getOptions,
+  editOption,
+  deleteOption,
+};
