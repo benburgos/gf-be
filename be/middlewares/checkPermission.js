28255314fb04f46a1df096c1cf2f485c9b08d400
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-14 11:34:13 -0700

    checks if product is active

--- be/middlewares/checkPermission.js
+++ be/middlewares/checkPermission.js
@@ -2,14 +2,14 @@ const Product = require('../models/sys/product');
 const Permission = require('../models/sys/permission');
 
 async function checkPermission(data) {
-  const { _id: prod } = await Product.findOne({
+  const { _id: prod, isActive: status } = await Product.findOne({
     brandId: data.bid,
     desc: data.prod,
   });
   const { pId: permId } = await data.ra.find((obj) => obj.productId === prod);
   const permission = await Permission.findOne({ _id: permId });
 
-  if (permission.brandId === data.bid && permission.productId === prod) {
+  if (status && permission.brandId === data.bid && permission.productId === prod) {
     return permission.type
   } else {
     return false
