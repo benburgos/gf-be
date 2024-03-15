20c608684c571d3fb5141c2a96ab8593ff9fd9e8
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-15 15:21:39 -0700

    adds name and desc attributes

--- be/models/app/qa/scorecard.js
+++ be/models/app/qa/scorecard.js
@@ -5,6 +5,8 @@ const scorecardSchema = new mongoose.Schema(
     _id: { type: String, required: true },
     brandId: { type: String, required: true },
     teamId: { type: String, required: true },
+    name:  { type: String, required: true },
+    desc:  { type: String, required: true },
     type: { type: String, enum: ['native', 'zendesk', 'salesforce'], required: true },
     modality: { type: String, required: true },
     criteria: { type: Array, required: true },
