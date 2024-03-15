99278ae8d0af519abd7e5985596cb91412f93cf9
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-15 15:44:40 -0700

    tests createScorecard

--- be/controllers/app/qa/scorecard.js
+++ be/controllers/app/qa/scorecard.js
@@ -2,11 +2,28 @@ const { Scorecard } = require('../../../models/app/qa/qaIndex');
 const { v4: uuidv4 } = require('uuid');
 const { checkPermission } = require('../../../middlewares/checkPermission');
 
-async function createScorecard(req, res){}
+async function createScorecard(req, res){
+    let newScorecard = {
+        ...req.body,
+        _id: uuidv4(),
+        brandId: req.bid,
+        teamId: "null",
+        isActive: true,
+        dateUpdated: Date.now(),
+        dateCreated: Date.now()
+    }
+
+    res.send(newScorecard)
+}
+
 async function getScorecard(req, res){}
+
 async function getAllScorecards(req, res){}
+
 async function editScorecard(req, res){}
+
 async function deleteScorecard(req, res){}
+
 
 module.exports = {
     createScorecard,
