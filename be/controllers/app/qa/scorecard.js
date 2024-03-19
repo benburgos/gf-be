18b0857246b6b744e8ab16fd4ba9855c6a62ecf1
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-19 14:21:24 -0700

    adds rbac to getScorecard

--- be/controllers/app/qa/scorecard.js
+++ be/controllers/app/qa/scorecard.js
@@ -34,8 +34,27 @@ async function createScorecard(req, res) {
 }
 
 async function getScorecard(req, res) {
-  let foundScorecard = await Scorecard.find({ _id: req.params.id });
-  res.send(foundScorecard)
+  const data = {
+    prod: 'qa',
+    bid: req.bid,
+    ra: req.ra,
+  };
+
+  const type = await checkPermission(data);
+
+  if (type === 'rw' || 'w') {
+    const foundScorecard = await Scorecard.findOne({ _id: req.params.id });
+
+    if (foundScorecard && foundScorecard.brandId === req.bid) {
+      res.json(foundScorecard);
+    } else if (foundScorecard && foundScorecard.brandId !== req.bid) {
+      res.send(`You do not belong to the same organization as this option.`);
+    } else {
+      res.send(`Scorecard ID does not exist.`);
+    }
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
 }
 
 async function getAllScorecards(req, res) {}
