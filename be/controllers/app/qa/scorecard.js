818d46156bb15405de8e0e36d49fa69f1a4fab35
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-19 14:18:28 -0700

    adds rbac to create scorecard

--- be/controllers/app/qa/scorecard.js
+++ be/controllers/app/qa/scorecard.js
@@ -2,33 +2,49 @@ const { Scorecard } = require('../../../models/app/qa/qaIndex');
 const { v4: uuidv4 } = require('uuid');
 const { checkPermission } = require('../../../middlewares/checkPermission');
 
-async function createScorecard(req, res){
-    let newScorecard = {
-        ...req.body,
-        _id: uuidv4(),
-        brandId: req.bid,
-        teamId: "null",
-        isActive: true,
-        dateUpdated: Date.now(),
-        dateCreated: Date.now()
-    }
+async function createScorecard(req, res) {
+  const data = {
+    prod: 'qa',
+    bid: req.bid,
+    ra: req.ra,
+  };
 
-    res.send(newScorecard)
+  const type = await checkPermission(data);
+
+  if (type === 'rw') {
+    let newScorecard = {
+      ...req.body,
+      _id: uuidv4(),
+      brandId: req.bid,
+      teamId: 'null',
+      isActive: true,
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    };
+
+    try {
+      await Scorecard.create(newScorecard);
+      res.send(newScorecard);
+    } catch (err) {
+      res.send(err);
+    }
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
 }
 
-async function getScorecard(req, res){}
+async function getScorecard(req, res) {}
 
-async function getAllScorecards(req, res){}
+async function getAllScorecards(req, res) {}
 
-async function editScorecard(req, res){}
+async function editScorecard(req, res) {}
 
-async function deleteScorecard(req, res){}
-
+async function deleteScorecard(req, res) {}
 
 module.exports = {
-    createScorecard,
-    getScorecard,
-    getAllScorecards,
-    editScorecard,
-    deleteScorecard
-}
+  createScorecard,
+  getScorecard,
+  getAllScorecards,
+  editScorecard,
+  deleteScorecard,
+};
