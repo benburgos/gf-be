672d353217a9a363d3e17a05d4bb9349f536e953
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-19 15:22:03 -0700

    creates pending evaluation

--- be/controllers/app/qa/evaluation.js
+++ be/controllers/app/qa/evaluation.js
@@ -1,18 +1,61 @@
 const { Evaluation } = require('../../../models/app/qa/qaIndex');
+const { Scorecard } = require('../../../models/app/qa/qaIndex');
 const { v4: uuidv4 } = require('uuid');
 const { checkPermission } = require('../../../middlewares/checkPermission');
 
-async function createEvaluation(req, res){}
-async function getEvaluation(req, res){}
-async function getAllEvaluations(req, res){}
-async function editEvaluation(req, res){}
-async function deleteEvaluation(req, res){}
+async function createEvaluation(req, res) {
+  const data = {
+    prod: 'qa',
+    bid: req.bid,
+    ra: req.ra,
+  };
+
+  const type = await checkPermission(data);
+
+  if (type === 'rw' || 'w') {
+    const evaluations = await Evaluation.countDocuments({ brandId: req.bid });
+
+    if (req.query.system === 'native') {
+      let pendingEvaluation = {
+        _id: uuidv4(),
+        brandId: req.bid,
+        evalId: evaluations + 1,
+        systemId: evaluations + 1,
+        modality: 'all',
+        userId: 'TBD',
+        teamId: 'TBD',
+        evaluatorId: req.id,
+        scorecardId: 'TBD',
+        score: 0,
+        targetScore: 0,
+        maxScore: 0,
+        evalType: req.query.type,
+        status: 'not_started',
+        data: [],
+        positiveFeedback: 'TBD',
+        improvementFeedback: 'TBD',
+        dateUpdated: Date.now(),
+        dateCreated: Date.now(),
+      };
+
+      await Evaluation.create(pendingEvaluation);
+      res.send(pendingEvaluation);
+    } else {
+      res.send(`Unable to determine evaluation system.`);
+    }
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
+}
+async function getEvaluation(req, res) {}
+async function getAllEvaluations(req, res) {}
+async function editEvaluation(req, res) {}
+async function deleteEvaluation(req, res) {}
 
 module.exports = {
-    createEvaluation,
-    getEvaluation,
-    getAllEvaluations,
-    editEvaluation,
-    deleteEvaluation
-}
-
+  createEvaluation,
+  getEvaluation,
+  getAllEvaluations,
+  editEvaluation,
+  deleteEvaluation,
+};
