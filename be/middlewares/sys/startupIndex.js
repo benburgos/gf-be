a063494f75d40dc5c9e575e727fcdb7fbd5c3466
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-20 15:52:13 -0700

    creates function index

---
+++ be/middlewares/sys/startupIndex.js
@@ -0,0 +1,11 @@
+const createBrand = require('./createBrand');
+const createProduct = require('./createProduct');
+const createPermission = require('./createPermission');
+const createRole = require('./createRole');
+
+module.exports = {
+  createBrand,
+  createProduct,
+  createPermission,
+  createRole,
+};
