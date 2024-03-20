2e06d1fa577d6b5f4208dc466b7edcf974ea1ef2
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-20 14:17:50 -0700

    adds userCount to getAllTeams

--- be/controllers/app/team.js
+++ be/controllers/app/team.js
@@ -1,4 +1,5 @@
 const Team = require('../../models/team');
+const User = require('../../models/user');
 const { v4: uuidv4 } = require('uuid');
 const { checkPermission } = require('../../middlewares/checkPermission');
 
@@ -32,29 +33,51 @@ async function createTeam(req, res) {
 }
 
 async function getTeam(req, res) {
-    const data = {
-        prod: 'admin',
-        bid: req.bid,
-        ra: req.ra,
-      };
-      const type = await checkPermission(data);
-    
-      if (type === 'r' || 'w' || 'rw') {
-        const foundTeam = await Team.findOne({ _id: req.params.id });
-    
-        if (foundTeam && foundTeam.brandId === req.bid) {
-          res.json(foundTeam);
-        } else if (foundTeam && foundTeam.brandId !== req.bid) {
-          res.send(`You do not belong to the same organization as this team.`);
-        } else {
-          res.send(`Team does not exist.`);
-        }
-      } else {
-        res.send(`You are not authorized to access this resource.`);
-      }
+  const data = {
+    prod: 'admin',
+    bid: req.bid,
+    ra: req.ra,
+  };
+  const type = await checkPermission(data);
+
+  if (type === 'r' || 'w' || 'rw') {
+    const foundTeam = await Team.findOne({ _id: req.params.id });
+
+    if (foundTeam && foundTeam.brandId === req.bid) {
+      res.json(foundTeam);
+    } else if (foundTeam && foundTeam.brandId !== req.bid) {
+      res.send(`You do not belong to the same organization as this team.`);
+    } else {
+      res.send(`Team does not exist.`);
+    }
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
 }
 
-async function getAllTeams(req, res) {}
+async function getAllTeams(req, res) {
+  const data = {
+    prod: 'admin',
+    bid: req.bid,
+    ra: req.ra,
+  };
+  const type = await checkPermission(data);
+
+  if (type === 'r' || 'w' || 'rw') {
+    let teams = await Team.find({ brandId: req.bid }, '_id name desc isActive');
+
+    for (let i = 0; i < teams.length; i++) {
+      let teamCount = await User.countDocuments({
+        brandId: req.bid,
+        teamId: teams[i]._id,
+      });
+      teams[i].userCount = teamCount;
+    }
+    res.send(teams);
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
+}
 
 async function editTeam(req, res) {}
 
