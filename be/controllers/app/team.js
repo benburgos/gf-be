6f80d903d34f5c836da3deef39cddd3dff20e0f5
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-20 13:48:13 -0700

    adds rbac to createTeam

--- be/controllers/app/team.js
+++ be/controllers/app/team.js
@@ -1,17 +1,48 @@
-const Team = require('../../models/team')
+const Team = require('../../models/team');
 const { v4: uuidv4 } = require('uuid');
 const { checkPermission } = require('../../middlewares/checkPermission');
 
-async function createTeam(req, res){}
-async function getTeam(req, res){}
-async function getAllTeams(req, res){}
-async function editTeam(req, res){}
-async function deleteTeam(req, res){}
+async function createTeam(req, res) {
+  const data = {
+    prod: 'admin',
+    bid: req.bid,
+    ra: req.ra,
+  };
+  const type = await checkPermission(data);
+
+  if (type === 'w' || 'rw') {
+    req.body = {
+      ...req.body,
+      _id: uuidv4(),
+      brandId: req.bid,
+      isActive: true,
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    };
+
+    try {
+      const team = await Team.create(req.body);
+      res.send(`New team, ${team.name}, was added to the database.`);
+    } catch (err) {
+      res.send(err);
+    }
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
+}
+
+async function getTeam(req, res) {}
+
+async function getAllTeams(req, res) {}
+
+async function editTeam(req, res) {}
+
+async function deleteTeam(req, res) {}
 
 module.exports = {
-    createTeam,
-    getTeam,
-    getAllTeams,
-    editTeam,
-    deleteTeam
-}
+  createTeam,
+  getTeam,
+  getAllTeams,
+  editTeam,
+  deleteTeam,
+};
