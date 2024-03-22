c4ea1db85ba15266aab66f471eabd6ad02c94c4f
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-22 09:58:19 -0700

    adds org details to user

--- be/models/user.js
+++ be/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+/// Add OrgId - rework team into Org, needs both a team and an org to belong to (e.g. Customer Service is in Operations), org object/model will have a team ID and a group ID
+
 const userSchema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
@@ -9,6 +11,12 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: false },
     phoneNumber: { type: String, required: false },
     roleId: { type: String, required: true },
+    org: {
+      orgId: { type: String, required: true },
+      orgName: { type: String, required: true },
+      teamId: { type: String, required: true },
+      teamName: { type: String, required: true },
+    },
     isActive: { type: Boolean, default: true, required: true },
     dateUpdated: { type: Number, required: true },
     dateCreated: { type: Number, required: true },
