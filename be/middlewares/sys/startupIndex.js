04981160740cb636996f6b081144745ff4548ba3
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-22 10:36:24 -0700

    adds createOrg and createTeam middlewares to index

--- be/middlewares/sys/startupIndex.js
+++ be/middlewares/sys/startupIndex.js
@@ -1,10 +1,14 @@
 const createBrand = require('./createBrand');
+const createOrg = require('./createOrg');
+const createTeam = require('./createTeam');
 const createProduct = require('./createProduct');
 const createPermission = require('./createPermission');
 const createRole = require('./createRole');
 
 module.exports = {
   createBrand,
+  createOrg,
+  createTeam,
   createProduct,
   createPermission,
   createRole,
