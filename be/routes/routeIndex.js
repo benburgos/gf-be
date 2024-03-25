21a87de6c64e8d6060e76bac6aa30bd470e5244f
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-25 12:57:34 -0700

    adds role to route index

--- be/routes/routeIndex.js
+++ be/routes/routeIndex.js
@@ -1,23 +1,25 @@
 const login = require('./login');
 const brand = require('./brand');
 const appUser = require('./app/user');
-const appOrg = require('./app/org')
+const appRole = require('./app/role');
+const appOrg = require('./app/org');
 const appTeam = require('./app/team');
 const appQaOption = require('./app/qa/option');
 const appQaQuestion = require('./app/qa/question');
-const appQaSection = require('./app/qa/section')
-const appQaScorecard = require('./app/qa/scorecard')
-const appQaEvaluation = require('./app/qa/evaluation')
+const appQaSection = require('./app/qa/section');
+const appQaScorecard = require('./app/qa/scorecard');
+const appQaEvaluation = require('./app/qa/evaluation');
 
 module.exports = {
   login,
   brand,
   appUser,
+  appRole,
   appOrg,
   appTeam,
   appQaOption,
   appQaQuestion,
   appQaSection,
   appQaScorecard,
-  appQaEvaluation
+  appQaEvaluation,
 };
