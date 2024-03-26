347f20d191b1f9ad0b1fe12c27a0a1f00b50fec6
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-26 15:55:16 -0700

    completes createScorecard

---
+++ be/middlewares/sys/qa/createScorecard.js
@@ -0,0 +1,38 @@
+const { Scorecard } = require('../../../models/app/qa/qaIndex');
+const { Team } = require('../../../models/team')
+const template = require('../../../middlewares/sys/qa/template/scorecards.json');
+const { v4: uuidv4 } = require('uuid');
+
+async function createScorecard(data) {
+  let newScorecardArr = [];
+  let team = await Team.findOne({brandId: data._id, name: "Unassigned"})
+
+  for (let i = 0; i < template.length; i++) {
+    let newScorecard = {
+      _id: uuidv4(),
+      brandId: data._id,
+      teamId: team._id,
+      name: template[i].name,
+      desc: template[i].desc,
+      type: template[i].type,
+      modality: template[i].modality,
+      criteria: template[i].criteria,
+      maxScore: template[i].maxScore,
+      targetScore: template[i].targetScore,
+      isActive: template[i].isActive,
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    };
+
+    newScorecardArr.push(newScorecard);
+  }
+
+  let createdScorecards = await Scorecard.insertMany(newScorecardArr);
+  console.log(
+    `${createdScorecards.length} QA scorecards were added to company, ${data.name}.`
+  );
+}
+
+module.exports = {
+  createScorecard,
+};
