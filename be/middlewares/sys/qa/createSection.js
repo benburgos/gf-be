fb4fb40a55756af6afba23250671d2d015b40918
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-26 15:45:43 -0700

    completes createSection

---
+++ be/middlewares/sys/qa/createSection.js
@@ -0,0 +1,33 @@
+const { Section } = require('../../../models/app/qa/qaIndex');
+const template = require('../../../middlewares/sys/qa/template/sections.json');
+const { v4: uuidv4 } = require('uuid');
+
+async function createSection(data) {
+  let newSectionArr = [];
+
+  for (let i = 0; i < template.length; i++) {
+    let newSection = {
+      _id: uuidv4(),
+      brandId: data._id,
+      name: template[i].name,
+      desc: template[i].desc,
+      modality: template[i].modality,
+      isActive: template[i].isActive,
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    };
+
+    newSectionArr.push(newSection);
+  }
+
+  let createdSections = await Section.insertMany(newSectionArr);
+  console.log(
+    `${createdSections.length} QA sections were added to company, ${data.name}.`
+  );
+
+  return newSectionArr;
+}
+
+module.exports = {
+  createSection,
+};
