311a15742eab819e610edcf4da5b407c36884a42
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-27 15:45:18 -0700

    adds brand admin check

--- be/controllers/app/product.js
+++ be/controllers/app/product.js
@@ -1,21 +1,37 @@
-const Brand = require('../../models/sys/brand')
+const Brand = require('../../models/sys/brand');
 const Product = require('../../models/sys/product');
 const { checkPermission } = require('../../middlewares/checkPermission');
 
 async function getAllProducts(req, res) {
-    const brandCheck = await Brand.findOne({_id: req.bid}, 'adminId')
-    console.log(brandCheck)
-    //   if (type === 'rw') {
-    //     let products = await Product.find({ brandId: req.bid }, '_id desc isActive');
-    //     res.send(products);
-    //   } else {
-    //     res.send(`You are not authorized to access this resource.`);
-    //   }
+  const brandCheck = await Brand.findOne({ _id: req.bid }, '-_id adminId');
+
+  if (brandCheck.adminId === req.id) {
+    let products = await Product.find(
+      { brandId: req.bid },
+      '_id desc isActive'
+    );
+    res.send(products);
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
 }
 
 async function editProduct(req, res) {
-    const brandCheck = await Brand.findOne({_id: req.bid}, 'adminId')
-    console.log(brandCheck)
+  const brandCheck = await Brand.findOne({ _id: req.bid }, 'adminId');
+
+  if (brandCheck.adminId === req.id) {
+    let product = await Product.findOneAndUpdate(
+      { _id: req.params.id, brandId: req.bid },
+      { isActive: req.query.update }
+    );
+    res.send(
+      `${product.desc} product has been ${
+        req.query.update === 'true' ? 'activated' : 'deactivated'
+      }.`
+    );
+  } else {
+    res.send(`You are not authorized to access this resource.`);
+  }
 }
 
 module.exports = {
