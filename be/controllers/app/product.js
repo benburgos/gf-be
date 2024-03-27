e5f62230fbbed9dde814226c585f58098eff52b0
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-27 15:32:40 -0700

    completes getAllProducts

--- be/controllers/app/product.js
+++ be/controllers/app/product.js
@@ -1,12 +1,25 @@
 const Product = require('../../models/sys/product');
 const { checkPermission } = require('../../middlewares/checkPermission');
 
-async function getProduct(req, res) {}
-async function getAllProducts(req, res) {}
+async function getAllProducts(req, res) {
+    const data = {
+        prod: 'admin',
+        bid: req.bid,
+        ra: req.ra,
+      };
+      const type = await checkPermission(data);
+    
+      if (type === 'rw') {
+        let products = await Product.find({ brandId: req.bid }, '_id desc isActive');
+        res.send(products);
+      } else {
+        res.send(`You are not authorized to access this resource.`);
+      }
+}
+
 async function editProduct(req, res) {}
 
 module.exports = {
-  getProduct,
   getAllProducts,
   editProduct,
 };
