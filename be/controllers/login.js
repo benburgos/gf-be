6ba466f4f2853e485492f9bfe98a8c11493f8495
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-27 16:51:39 -0700

    adds note

--- be/controllers/login.js
+++ be/controllers/login.js
@@ -1,6 +1,7 @@
 const { checkHash } = require('../middlewares/checkHash');
 const { genToken } = require('../services/genToken');
 
+// update this function to redirect to the app/login route if they are not the company admin. If the company admin logs in on this route, redirect to the company page. 
 async function loginUser(req, res) {
   try {
     const match = await checkHash(req.body.email, req.body.password);
