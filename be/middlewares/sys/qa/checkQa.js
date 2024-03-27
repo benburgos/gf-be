49990712e3d0d4c3c9a2f04837a72043d601e95d
Ben Burgos <ben.a.burgos@gmail.com>
2024-03-27 16:16:04 -0700

    adds qa check and start

--- be/middlewares/sys/qa/checkQa.js
+++ be/middlewares/sys/qa/checkQa.js
@@ -1,4 +1,17 @@
+const qa = require('../../../models/app/qa/qaIndex')
 
+async function checkQa(data){
+  let options = await qa.Option.countDocuments({brandId: data})
+  let questions = await qa.Question.countDocuments({brandId: data})
+  let sections = await qa.Section.countDocuments({brandId: data})
+  let scorecards = await qa.Scorecard.countDocuments({brandId: data})
+
+  if (options || questions || sections || scorecards > 0){
+    return true
+  } else {
+    return false
+  }
+}
 
 module.exports = {
   checkQa
--- be/middlewares/sys/qa/startQa.js
+++ be/middlewares/sys/qa/startQa.js
@@ -3,9 +3,13 @@ const { createQuestion } = require('./createQuestion');
 const { createSection } = require('./createSection');
 const { createScorecard } = require('./createScorecard');
 
+async function startQa(data) {
+  await createOption(data);
+  await createQuestion(data);
+  await createSection(data);
+  await createScorecard(data);
+}
+
 module.exports = {
-  createOption,
-  createQuestion,
-  createSection,
-  createScorecard,
+  startQa,
 };
