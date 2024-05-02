e4be837ed70b1cdbd4e8311f1c5d256a2c5aae39
Ben <ben.a.burgos@gmail.com>
2024-05-02 13:10:12 -0700

    adds new logic to app login route

--- be/controllers/app/login.js
+++ be/controllers/app/login.js
@@ -1,19 +1,23 @@
-const { checkHash } = require('../../middlewares/checkHash');
-const { genToken } = require('../../services/genToken');
+const User = require('../models/user');
+const { checkHash } = require('../middlewares/checkHash');
+const { genToken } = require('../services/genToken');
 
-async function loginAppUser(req, res) {
-  try {
-    const match = await checkHash(req.body.email, req.body.password);
-    if (match) {
-      res.json(genToken(match, match.key));
-    } else {
-      res.send('Incorrect Password.');
+async function loginUser(req, res) {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    res.json('Email does not exist.');
+  } else if (user) {
+    try {
+      const match = await checkHash(user, req.body.password);
+      if (match) {
+        res.json(genToken(match, match.key));
+      }
+    } catch (err) {
+      throw err;
     }
-  } catch (err) {
-    throw err;
   }
 }
 
 module.exports = {
-  loginAppUser,
+  loginUser,
 };
