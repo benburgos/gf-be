3602184c3eb786cdea05c73fa22fbb311ab8657a
Ben <ben.a.burgos@gmail.com>
2024-05-02 12:12:21 -0700

    updates with error messaging

--- be/middlewares/checkHash.js
+++ be/middlewares/checkHash.js
@@ -5,7 +5,17 @@ const Role = require('../models/sys/role');
 
 async function checkHash(email, password) {
   const user = await User.findOne({ email: email });
+
+  if (!user) {
+    res.send('Email does not exist.');
+  }
+
   const pw = await Pwh.findOne({ userId: user._id });
+
+  if (!pw) {
+    res.send('Password is incorrect.');
+  }
+
   const role = await Role.findOne({ _id: user.roleId });
 
   const isMatch = await bcrypt.compare(password, pw.pwh);
