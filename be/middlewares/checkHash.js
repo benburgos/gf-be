c0d227d15a113edcf9fc0cb1fb5a10defff34223
Ben <ben.a.burgos@gmail.com>
2024-05-02 13:06:43 -0700

    updates formatting

--- be/middlewares/checkHash.js
+++ be/middlewares/checkHash.js
@@ -3,21 +3,20 @@ const Pwh = require('../models/pwh');
 const Role = require('../models/sys/role');
 
 async function checkHash(user, password) {
-    const pw = await Pwh.findOne({ userId: user._id });
-    const isMatch = await bcrypt.compare(password, pw.pwh);
-    if (isMatch) {
-      const role = await Role.findOne({ _id: user.roleId });
-        return {
-          id: user._id,
-          bid: user.brandId,
-          rid: user.roleId,
-          ra: role.permissions,
-        };
-      } else {
-        return 'Incorrect Password.';
-      }
-    }
-
+  const pw = await Pwh.findOne({ userId: user._id });
+  const isMatch = await bcrypt.compare(password, pw.pwh);
+  if (isMatch) {
+    const role = await Role.findOne({ _id: user.roleId });
+    return {
+      id: user._id,
+      bid: user.brandId,
+      rid: user.roleId,
+      ra: role.permissions,
+    };
+  } else {
+    return 'Incorrect Password.';
+  }
+}
 
 module.exports = {
   checkHash,
