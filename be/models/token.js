c8a4b881d01d89122e3a7da066b2874df42599de
Ben <ben.a.burgos@gmail.com>
2024-05-06 15:05:19 -0700

    adds refresh token model

---
+++ be/models/token.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const refreshTokenSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    token: { type: String, required: true },
+    dateUpdated: { type: Number, required: true },
+    dateCreated: { type: Number, required: true },
+  },
+  { versionKey: false }
+);
+
+const RefreshToken = mongoose.model('RefreshToken', refreshTokenSchema);
+
+module.exports = Team;
