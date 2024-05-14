0ac831d861088b3a972dd1c63d916621e5a62cc9
Ben <ben.a.burgos@gmail.com>
2024-05-14 15:17:54 -0700

    updates function to query userData and roleData

--- be/controllers/api/user.js
+++ be/controllers/api/user.js
@@ -1,15 +1,36 @@
 const User = require('../../models/user');
-const Pwh = require('../../models/pwh');
+// const Pwh = require('../../models/pwh');
 const Role = require('../../models/sys/role');
-const { v4: uuidv4 } = require('uuid');
-const { hashPassword } = require('../../middlewares/genHash');
-const { checkEmail } = require('../../services/checkEmail');
-const { checkPermission } = require('../../middlewares/checkPermission');
+// const { v4: uuidv4 } = require('uuid');
+// const { hashPassword } = require('../../middlewares/genHash');
+// const { checkEmail } = require('../../services/checkEmail');
+// const { checkPermission } = require('../../middlewares/checkPermission');
 
 async function getUser(req, res) {
-  
+  try {
+    // Assuming userId is passed in the request parameters
+    const userId = req.params.id;
+
+    // Retrieve user from the database
+    const userData = await User.findById(
+      userId,
+      '-_id -brandId -org.orgId -org.teamId'
+    );
+    const roleData = await Role.findById(userData.roleId, 'name -_id');
+
+    // Check if user exists
+    if (!userData || userData.isActive === false) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // If user exists, return user data
+    return res.json({user: userData, role: roleData});
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 }
 
 module.exports = {
-  getUser
+  getUser,
 };
