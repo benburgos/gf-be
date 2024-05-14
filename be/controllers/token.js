2759a1158ece61a20524b00aee3b740a09d0c5cc
Ben <ben.a.burgos@gmail.com>
2024-05-14 13:39:13 -0700

    updates refresh token logic

--- be/controllers/token.js
+++ be/controllers/token.js
@@ -1,4 +1,6 @@
 const { verifyRefreshToken } = require('../middlewares/verifyRefreshToken');
+const User = require('../models/user');
+const Role = require('../models/sys/role');
 const { genToken } = require('../services/genToken');
 
 // Controller function for refreshing tokens
@@ -9,20 +11,23 @@ async function refreshToken(req, res) {
 
     // Verify the refresh token
     const decoded = await verifyRefreshToken(refreshToken);
+    const user = await User.findOne({ _id: decoded.userId });
+    const role = await Role.findOne({ _id: user.roleId });
+
     if (!decoded) {
       return res.status(401).json({ error: 'Invalid refresh token' });
     }
 
     // Generate new access token
     const newAccessToken = genToken({
-      id: decoded.id,
-      bid: decoded.bid,
-      rid: decoded.rid,
-      ra: decoded.ra,
+      id: decoded.userId,
+      bid: user.brandId,
+      rid: user.roleId,
+      ra: role.permissions,
     });
 
     // Send the new access token to the client
-    res.json({ accessToken: newAccessToken.token });
+    res.json({ token: newAccessToken.token });
   } catch (error) {
     console.error('Error refreshing token:', error);
     res.status(500).json({ error: 'Internal server error' });
