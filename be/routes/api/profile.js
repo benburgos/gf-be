e1aa3dd90f19e3de5b5f06c29245a38c9d9c1228
Ben <ben.a.burgos@gmail.com>
2024-05-14 15:50:12 -0700

    removes routes for profile

--- be/routes/api/profile.js
+++ be/routes/api/profile.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const profileController = require('../../controllers/api/profile');
 const { authToken } = require('../../middlewares/authToken');
 
-// router.post('/create', userController.createUser);
 router.get('/:id', profileController.getUserProfile);
-// router.get('/', userController.getAllUsers);
 // router.put('/edit/:id', userController.editUser);
-// router.delete('/delete/:id', userController.deleteUser);
 
 module.exports = router;
