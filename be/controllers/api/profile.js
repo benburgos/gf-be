8c544b7021dae3dbef147f650a5a5c4ca9ddf20f
Ben <ben.a.burgos@gmail.com>
2024-05-14 16:26:38 -0700

    finished profile function

--- be/controllers/api/profile.js
+++ be/controllers/api/profile.js
@@ -9,7 +9,7 @@ async function getUserProfile(req, res) {
     // Retrieve user from the database
     let userData = await User.findById(
       userId,
-      '-_id -brandId -org.orgId -org.teamId'
+      '-_id -brandId -role.roleId -org.orgId -org.teamId'
     );
 
     // Check if user exists
@@ -17,35 +17,41 @@ async function getUserProfile(req, res) {
       return res.status(404).json({ Error: 'User not found' });
     }
 
-    // Retrieve role name from Role collection
-    let { name: roleName } = await Role.findById(userData.roleId, 'name -_id');
-
     // Retrieve manager details, if available
+    let managerData = {};
     if (userData.managerId.length > 0) {
-      let managerData = await User.findById(
+      managerData = await User.findById(
         userData.managerId,
-        'firstName lastName email phoneNumber location'
+        'firstName lastName email phoneNumber location role.roleName photoUrl'
       );
     }
 
     // Retrieve user's team members
     let teamMembers = await User.find(
-      { managerId: userId },
-      '-_id -brandId -org.orgId -org.teamId'
+      { managerId: managerData._id },
+      'firstName lastName role.roleName photoUrl'
     );
+
+    // Transforms Data
+    let data = {
+      user: {
+        ...userData._doc,
+        managerId: null,
+      },
+      manager: {
+        ...managerData._doc,
+      },
+      teamMembers: teamMembers
+      .filter(member => member._doc._id !== req.params.id)
+      .map(member => ({ ...member._doc })),
+    }
     
-    // Remove roleId and managerId from user data
-    userData = {
-      ...userData._doc,
-      roleId: null,
-      managerId: null,
-    };
 
     // If user exists, return user data
-    return res.json({ user: userData, role: roleName });
+    return res.json(data);
   } catch (error) {
     console.error('Error fetching user:', error);
-    return res.status(500).json({ Error: 'Internal server error' });
+    return res.status(500).json({ Error: 'Internal server error'});
   }
 }
 
