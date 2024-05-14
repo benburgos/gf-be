bce56bedb4e20a0fb93e2a8432c23bd07c156097
Ben <ben.a.burgos@gmail.com>
2024-05-14 13:12:15 -0700

    removes token

--- be/routes/token.js
+++ be/routes/token.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const tokenController = require('../controllers/token');
 
-router.post('/token', tokenController.refreshToken);
+router.post('/', tokenController.refreshToken);
 
 module.exports = router;
