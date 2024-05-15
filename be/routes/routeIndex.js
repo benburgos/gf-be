c54ce07e09c663da1d1026b1ed7c9dc685a8d55b
Ben <ben.a.burgos@gmail.com>
2024-05-15 10:40:59 -0700

    adds new admin routes to index

--- be/routes/routeIndex.js
+++ be/routes/routeIndex.js
@@ -4,6 +4,9 @@ const appToken = require('./token');
 const appLogin = require('./app/login');
 const appUserProfile = require('./app/profile');
 const appAdminUser = require('./app/admin/user');
+const appAdminRole = require('./app/admin/role');
+const appAdminOrg = require('./app/admin/org');
+const appAdminTeam = require('./app/admin/team');
 
 module.exports = {
   login,
@@ -12,4 +15,7 @@ module.exports = {
   appLogin,
   appUserProfile,
   appAdminUser,
+  appAdminRole,
+  appAdminOrg,
+  appAdminTeam,
 };
