2be2188dfb3e39697e65f328d30d4fe81f76d432
Ben <ben.a.burgos@gmail.com>
2024-05-15 10:27:40 -0700

    removes previous routes for refactoring and adds admin route

--- be/routes/routeIndex.js
+++ be/routes/routeIndex.js
@@ -2,32 +2,14 @@ const login = require('./login');
 const brand = require('./brand');
 const appToken = require('./token');
 const appLogin = require('./app/login');
-const appUser = require('./app/user');
 const appUserProfile = require('./app/profile');
-const appProduct = require('./app/product');
-const appRole = require('./app/role');
-const appOrg = require('./app/org');
-const appTeam = require('./app/team');
-const appQaOption = require('./app/qa/option');
-const appQaQuestion = require('./app/qa/question');
-const appQaSection = require('./app/qa/section');
-const appQaScorecard = require('./app/qa/scorecard');
-const appQaEvaluation = require('./app/qa/evaluation');
+const appAdminUser = require('./app/admin/user');
 
 module.exports = {
   login,
   brand,
   appToken,
   appLogin,
-  appUser,
   appUserProfile,
-  appProduct,
-  appRole,
-  appOrg,
-  appTeam,
-  appQaOption,
-  appQaQuestion,
-  appQaSection,
-  appQaScorecard,
-  appQaEvaluation,
+  appAdminUser,
 };
--- be/server.js
+++ be/server.js
@@ -18,17 +18,8 @@ app.use('/login', route.login);
 app.use('/brand', route.brand);
 app.use('/token', route.appToken);
 app.use('/app/login', route.appLogin);
-app.use('/app/profile', route.apiUserProfile);
-app.use('/app/user', route.appUser);
-app.use('/app/product', route.appProduct);
-app.use('/app/role', route.appRole);
-app.use('/app/team', route.appTeam);
-app.use('/app/org', route.appOrg);
-app.use('/app/qa/option', route.appQaOption);
-app.use('/app/qa/question', route.appQaQuestion);
-app.use('/app/qa/section', route.appQaSection);
-app.use('/app/qa/scorecard', route.appQaScorecard);
-app.use('/app/qa/evaluate', route.appQaEvaluation);
+app.use('/app/profile', route.appUserProfile);
+app.use('/app/admin/user', route.appAdminUser);
 
 // Listener
 app.listen(process.env.PORT, () =>
