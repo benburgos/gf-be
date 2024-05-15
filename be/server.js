d382ef2073403ccfca2ae6f8509c40ee75248bbe
Ben <ben.a.burgos@gmail.com>
2024-05-15 09:18:42 -0700

    updates routes for login and profile

--- be/server.js
+++ be/server.js
@@ -17,6 +17,8 @@ app.get('/', (req, res) => {
 app.use('/login', route.login);
 app.use('/brand', route.brand);
 app.use('/token', route.appToken);
+app.use('/app/login', route.appLogin);
+app.use('/app/profile', route.apiUserProfile);
 app.use('/app/user', route.appUser);
 app.use('/app/product', route.appProduct);
 app.use('/app/role', route.appRole);
@@ -27,19 +29,6 @@ app.use('/app/qa/question', route.appQaQuestion);
 app.use('/app/qa/section', route.appQaSection);
 app.use('/app/qa/scorecard', route.appQaScorecard);
 app.use('/app/qa/evaluate', route.appQaEvaluation);
-
-/////// Future Additions ///////////////////////////////
-app.use('/api/login', route.appLogin);
-app.use('/api/profile', route.apiUserProfile);
-// app.use('/api/qa', route.apiQa)
-// app.use('/app/performance', route.appPerformance)
-// app.use('/api/performance', route.apiPerformance)
-// app.use('/app/engagement', route.appEngagement)
-// app.use('/api/engagement', route.apiEngagement)
-// app.use('/app/wfm', route.appWfm)
-// app.use('/api/wfm', route.apiWfm)
-// app.use('/app/shop', route.appShop)
-// app.use('/api/shop', route.apiShop)
 
 // Listener
 app.listen(process.env.PORT, () =>
