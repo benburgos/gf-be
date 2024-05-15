908648cd1803124df1e0ab3647250622a5bd2305
Ben <ben.a.burgos@gmail.com>
2024-05-15 16:34:22 -0700

    adds commentary for readability

--- be/middlewares/sys/createProduct.js
+++ be/middlewares/sys/createProduct.js
@@ -1,30 +1,39 @@
 const Product = require('../../models/sys/product');
 const { v4: uuidv4 } = require('uuid');
 
-async function createProduct(data) {
+async function createProduct(brandId) {
   try {
+    // List of products
     const productList = ['qa', 'pm', 'eng', 'wfm', 'shop', 'admin'];
     const products = [];
 
+    // Loop through each product in the list
     for (let i = 0; i < productList.length; i++) {
+      // Create product object
       const product = {
         _id: uuidv4(),
-        brandId: data,
+        brandId: brandId,
         desc: productList[i],
         dateUpdated: Date.now(),
         dateCreated: Date.now(),
       };
 
+      // Set isActive to true for admin product as default product
       if (product.desc === 'admin') {
         product.isActive = true;
       }
 
+      // Push product to products array
       products.push(product);
     }
 
+    // Insert products into database
     await Product.insertMany(products);
+
+    // Return the array of created product objects
     return products;
   } catch (error) {
+    // Handle errors
     console.error('Error creating products:', error);
     throw new Error('Failed to create products');
   }
