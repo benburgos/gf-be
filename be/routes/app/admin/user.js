610a60912be02d6e3364e954276f1aa60ba25908
Ben <ben.a.burgos@gmail.com>
2024-05-15 09:31:41 -0700

    updates import names

--- be/routes/app/admin/user.js
+++ be/routes/app/admin/user.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const adminUserController = require('../../../controllers/app/admin/user');
 const { authToken } = require('../../../middlewares/authToken');
 
-router.post('/create', adminUserController.createUser);
-router.get('/', adminUserController.getUsers);
-router.get('/:id', adminUserController.getUser);
-router.put('/:id/edit', adminUserController.getUser);
+router.post('/create', adminUserController.adminCreateUser);
+router.get('/:id', adminUserController.adminGetUser);
+router.get('/', adminUserController.adminGetUsers);
+router.put('/:id/edit', adminUserController.adminEditUser);
 
 module.exports = router;
