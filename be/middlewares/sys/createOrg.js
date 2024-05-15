f429b54b7a881d16a8c5b38ec1cdaec7a48a19b8
Ben <ben.a.burgos@gmail.com>
2024-05-15 16:22:14 -0700

    refactors code

--- be/middlewares/sys/createOrg.js
+++ be/middlewares/sys/createOrg.js
@@ -2,31 +2,39 @@ const Org = require('../../models/org');
 const { v4: uuidv4 } = require('uuid');
 
 async function createOrg(data) {
-  const orgsToCreate = [
-    (unassignedOrg = {
-      _id: uuidv4(),
-      brandId: data._id,
-      name: 'Unassigned',
-      desc: 'Users without a current org assignment.',
-      dateUpdated: Date.now(),
-      dateCreated: Date.now(),
-    }),
-    (adminOrg = {
-      _id: uuidv4(),
-      brandId: data._id,
-      name: 'System Admin',
-      desc: 'Company administrator user org.',
-      dateUpdated: Date.now(),
-      dateCreated: Date.now(),
-    }),
-  ]
+  try {
+    // Define orgs to create
+    const orgsToCreate = [
+      {
+        _id: uuidv4(),
+        brandId: data,
+        name: 'Unassigned',
+        desc: 'Users without a current org assignment.',
+        dateUpdated: Date.now(),
+        dateCreated: Date.now(),
+      },
+      {
+        _id: uuidv4(),
+        brandId: data,
+        name: 'System Admin',
+        desc: 'Company administrator user org.',
+        dateUpdated: Date.now(),
+        dateCreated: Date.now(),
+      }
+    ];
 
-  await Org.insertMany(orgsToCreate)
-  console.log(
-    `${orgsToCreate.length} orgs were added to company, ${data.name}.`
-  );
+    // Insert orgs into database
+    await Org.insertMany(orgsToCreate);
 
-  return orgsToCreate[1]
+    console.log(`${orgsToCreate.length} orgs were added to company.`);
+    
+    // Find the org for 'System Admin' and return the _id and name
+    const systemAdminOrg = orgsToCreate.find(org => org.name === 'System Admin');
+    return { orgId: systemAdminOrg._id, orgName: systemAdminOrg.name };
+  } catch (error) {
+    console.error('Error creating orgs:', error);
+    throw new Error('Failed to create orgs');
+  }
 }
 
 module.exports = {
