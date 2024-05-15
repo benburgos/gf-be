849dbeddee794c84a659aa43d3aa9362a51da62f
Ben <ben.a.burgos@gmail.com>
2024-05-15 16:48:33 -0700

    breaks out the creation of user into separate function

---
+++ be/middlewares/sys/createUser.js
@@ -0,0 +1,31 @@
+const User = require('../../models/user');
+const Pwh = require('../../models/pwh');
+const { hashPassword } = require('../../middlewares/genHash');
+
+async function createUser(userData, plainPassword) {
+  try {
+    // Hash the password
+    const hashedPassword = await hashPassword(plainPassword);
+
+    // Create user document
+    const user = await User.create(userData);
+
+    // Create Pwh document for storing hashed password
+    const pwh = await Pwh.create({
+      _id: user._id, // Use the user ID as the Pwh document ID
+      userId: user._id,
+      pwh: hashedPassword,
+      dateUpdated: Date.now(),
+      dateCreated: Date.now(),
+    });
+
+    return { user, pwh }; // Return both the created user and pwh documents
+  } catch (error) {
+    console.error('Error creating user:', error);
+    throw new Error('Failed to create user');
+  }
+}
+
+module.exports = {
+  createUser,
+};
