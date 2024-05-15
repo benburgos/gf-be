53db19c1dfaacda5d001572aad2cb120842f9cda
Ben <ben.a.burgos@gmail.com>
2024-05-15 10:19:25 -0700

    adds authtoken middleware

--- be/routes/app/admin/user.js
+++ be/routes/app/admin/user.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const adminUserController = require('../../../controllers/app/admin/user');
 const { authToken } = require('../../../middlewares/authToken');
 
-router.post('/create', adminUserController.adminCreateUser);
-router.get('/:id', adminUserController.adminGetUser);
-router.get('/', adminUserController.adminGetUsers);
-router.put('/:id/edit', adminUserController.adminEditUser);
+router.post('/create', authToken, adminUserController.adminCreateUser);
+router.get('/:id', authToken, adminUserController.adminGetUser);
+router.get('/', authToken, adminUserController.adminGetUsers);
+router.put('/:id/edit', authToken, adminUserController.adminEditUser);
 
 module.exports = router;
