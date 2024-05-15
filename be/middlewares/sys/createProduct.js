6690dea16dbe1da8b0aab0aaa3422ea153f5fb4e
Ben <ben.a.burgos@gmail.com>
2024-05-15 16:28:09 -0700

    refactors for better error handling

--- be/middlewares/sys/createProduct.js
+++ be/middlewares/sys/createProduct.js
@@ -2,30 +2,32 @@ const Product = require('../../models/sys/product');
 const { v4: uuidv4 } = require('uuid');
 
 async function createProduct(data) {
-  const productList = ['qa', 'pm', 'eng', 'wfm', 'shop', 'admin'];
-  const products = [];
+  try {
+    const productList = ['qa', 'pm', 'eng', 'wfm', 'shop', 'admin'];
+    const products = [];
 
-  for (let i = 0; i < productList.length; i++) {
-    const product = {
-      _id: uuidv4(),
-      brandId: data._id,
-      desc: productList[i],
-      dateUpdated: Date.now(),
-      dateCreated: Date.now(),
-    };
+    for (let i = 0; i < productList.length; i++) {
+      const product = {
+        _id: uuidv4(),
+        brandId: data,
+        desc: productList[i],
+        dateUpdated: Date.now(),
+        dateCreated: Date.now(),
+      };
 
-    if (product.desc === 'admin') {
-      product.isActive = true;
+      if (product.desc === 'admin') {
+        product.isActive = true;
+      }
+
+      products.push(product);
     }
 
-    products.push(product);
+    await Product.insertMany(products);
+    return products;
+  } catch (error) {
+    console.error('Error creating products:', error);
+    throw new Error('Failed to create products');
   }
-
-  await Product.insertMany(products);
-  console.log(
-    `${products.length} products were added to company, ${data.name}.`
-  );
-  return products;
 }
 
 module.exports = {
