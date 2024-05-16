155bb9c7c467a0c4dd5fd3a680f2a5b465f34093
Ben <ben.a.burgos@gmail.com>
2024-05-16 12:47:52 -0700

    updates brand login

--- be/controllers/login.js
+++ be/controllers/login.js
@@ -5,28 +5,36 @@ const { genToken } = require('../services/genToken');
 const { genRefreshToken } = require('../services/genRefreshToken');
 
 async function loginUser(req, res) {
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) {
-    res.json('Email does not exist.');
-  } else if (user) {
-    const match = await checkHash(user, req.body.password);
-    if (match) {
-      const isAdmin = await Brand.findOne({ adminId: match.id });
-      if (isAdmin !== null) {
-        // Generate access token
-        const accessToken = genToken(match, match.key);
-
-        // Generate refresh token
-        const refreshToken = await genRefreshToken(user._id);
-
-        // Send tokens to the client
-        return res.json({ accessToken, refreshToken });
-      } else {
-        res.json('Use the app login page to access the app.');
-      }
-    } else {
-      res.json('Password is incorrect.');
+  try {
+    // Retrieve the user from the database
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).json({ Message: 'User not found' });
     }
+
+    // Check if the user is the brand admin
+    const brand = await Brand.findOne({ adminId: user._id });
+    if (!brand) {
+      return res.status(401).json({ Message: 'You are not authorized to access this resource. Visit the app login!' });
+    }
+
+    // Check if the password matches
+    const match = await checkHash(user, req.body.password);
+    if (!match) {
+      return res.status(401).json({ Message: 'Password is incorrect' });
+    }
+
+    // Generate access token
+    const accessToken = genToken(match, match.key);
+
+    // Generate refresh token
+    const refreshToken = await genRefreshToken(user._id);
+
+    // Send tokens to the client
+    return res.json({ accessToken, refreshToken });
+} catch (error) {
+    console.error('Error logging in user:', error);
+    res.status(500).json({ Error: 'Internal server error' });
   }
 }
 
