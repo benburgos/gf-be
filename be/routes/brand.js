a35e432935d0bec20242d93b4ddcb26551dbffec
Ben <ben.a.burgos@gmail.com>
2024-05-16 12:13:15 -0700

    adds a brand admin user edit route

--- be/routes/brand.js
+++ be/routes/brand.js
@@ -6,7 +6,8 @@ const { authToken } = require('../middlewares/authToken');
 router.post('/create', brandController.newBrand);
 router.get('/:id', authToken, brandController.getBrand);
 router.put('/edit/:id', authToken, brandController.editBrand);
-router.put('/:id/products/activate', authToken, brandController.addProduct);
-router.put('/:id/products/deactivate', authToken, brandController.removeProduct);
+router.put('/edit/:id/user', authToken, brandController.editUser);
+router.put('/edit/:id/products/activate', authToken, brandController.addProduct);
+router.put('/edit/:id/products/deactivate', authToken, brandController.removeProduct);
 
 module.exports = router;
