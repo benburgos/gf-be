6b497c7c1c11dc6070d98693532385623aae69e0
Ben <ben.a.burgos@gmail.com>
2024-05-22 12:16:18 -0700

    adjusts getProfile for existing managerId

--- be/controllers/app/profile.js
+++ be/controllers/app/profile.js
@@ -16,40 +16,48 @@ async function getUserProfile(req, res) {
       return res.status(404).json({ Error: 'User not found' });
     }
 
+    // Initialize managerData as null
+    let managerData = null;
+
     // Retrieve manager details, if available
-    let managerData = {};
-    if (userData.managerId.length > 0) {
+    if (userData.managerId && userData.managerId.length > 0) {
       managerData = await User.findById(
         userData.managerId,
         'firstName lastName email phoneNumber location role.roleName photoUrl'
       );
     }
 
-    // Retrieve user's team members
-    let teamMembers = await User.find(
-      { managerId: managerData._id },
-      'firstName lastName role.roleName photoUrl'
-    );
+    // Initialize teamMembers as null
+    let teamMembers = null;
+
+    // Retrieve user's team members, if managerData exists
+    if (managerData) {
+      const members = await User.find(
+        { managerId: managerData._id },
+        'firstName lastName role.roleName photoUrl'
+      );
+
+      if (members.length > 0) {
+        teamMembers = members
+          .filter((member) => member._doc._id.toString() !== userId)
+          .map((member) => ({ ...member._doc }));
+      }
+    }
 
     // Transforms Data
     let data = {
       user: {
         ...userData._doc,
       },
-      manager: {
-        ...managerData._doc,
-      },
-      teamMembers: teamMembers
-      .filter(member => member._doc._id !== req.params.id)
-      .map(member => ({ ...member._doc })),
-    }
-    
+      manager: managerData ? { ...managerData._doc } : null,
+      teamMembers: teamMembers,
+    };
 
     // If user exists, return user data
     return res.json(data);
   } catch (error) {
     console.error('Error fetching user:', error);
-    return res.status(500).json({ Error: 'Internal server error'});
+    return res.status(500).json({ Error: 'Internal server error' });
   }
 }
 
