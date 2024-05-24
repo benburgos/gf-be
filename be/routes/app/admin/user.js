d141e0b57e018e219b68df3150e0aa61d726e356
Ben <ben.a.burgos@gmail.com>
2024-05-24 11:21:32 -0700

    creates options route

--- be/routes/app/admin/user.js
+++ be/routes/app/admin/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const adminUserController = require('../../../controllers/app/admin/user');
 const { authToken } = require('../../../middlewares/authToken');
 
+router.get('/options', authToken, adminUserController.adminCreateUserOptions);
 router.post('/create', authToken, adminUserController.adminCreateUser);
 router.get('/:id', authToken, adminUserController.adminGetUser);
 router.get('/', authToken, adminUserController.adminGetUsers);
